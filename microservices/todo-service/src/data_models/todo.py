ca9e6b8a939a1326d84d0f93cbb715b3aad65737
Anna Metz <aniametz@gmail.com>
2025-08-07 20:25:38 +0200

    Updated migrations folder, added new columns - priority, createdAt, completedAt

--- microservices/todo-service/src/data_models/todo.py
+++ microservices/todo-service/src/data_models/todo.py
@@ -1,13 +1,22 @@
-from sqlalchemy import Integer, String, Boolean
+from datetime import datetime, timezone
+from sqlalchemy import DateTime
 from sqlalchemy.orm import Mapped, mapped_column
 from database import db
 import uuid
 
+class Priority:
+    LOW = "low"
+    MEDIUM = "medium"
+    HIGH = "high"
+
 class Todo(db.Model):
     id: Mapped[str] = mapped_column(primary_key=True, default=lambda: str(uuid.uuid4()))
     description: Mapped[str] = mapped_column(unique=True)
+    priority: Mapped[str] = mapped_column(nullable=True)
     isDone: Mapped[bool]
     isArchived: Mapped[bool]
+    createdAt: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
+    completedAt: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=True)
 
     def as_dict(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
