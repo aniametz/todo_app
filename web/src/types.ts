3b6a7649890e90f8ce2ce760e2ee288d82f97840
Anna Metz <aniametz@gmail.com>
2025-08-08 23:12:09 +0200

    Added logic for new todo columns on web

--- web/src/types.ts
+++ web/src/types.ts
@@ -1,6 +1,35 @@
 export interface ToDo {
 	id?: string;
 	description: string;
+	priority?: Priority | null;
 	isDone: boolean;
 	isArchived: boolean;
+	createdAt?: string;
+	completedAt?: string;
 }
+
+export enum Priority {
+	Low = 'low',
+	Medium = 'medium',
+	High = 'high'
+}
+
+export type FlowbiteBadgeColor =
+	| 'none'
+	| 'red'
+	| 'yellow'
+	| 'green'
+	| 'indigo'
+	| 'purple'
+	| 'pink'
+	| 'blue'
+	| 'dark'
+	| 'primary'
+	| undefined;
+
+export const PriorityColor: Record<string | 'none', FlowbiteBadgeColor> = {
+	none: 'none',
+	[Priority.Low]: 'green',
+	[Priority.Medium]: 'yellow',
+	[Priority.High]: 'red'
+};
